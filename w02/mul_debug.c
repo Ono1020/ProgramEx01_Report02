2827c9c061dffe991aa6364f103701609bc5ac2c
Onosans <10ryou20ta5819@gmail.com>
2024-10-19 16:36:54 +0900

    [add]iroiro

--- w02/mul_debug.c
+++ w02/mul_debug.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdbool.h>
+
+bool mul_check(int a, int b, int n[32]);
 
 int main(void){
 	// int型の変数を配列で表現するために使う変数の用意
@@ -8,16 +11,36 @@ int main(void){
 		n[i] = 1 << i;
 	}
 
-
+	printf("start\n\n");
 	// 入力値の保存
 	int a, b; // a: 被乗数 b: 乗数
-	printf("被乗数と乗数を入力して下さい\n");
-	printf("被乗数:");
-	scanf("%d", &a);
-	printf("乗数:");
-	scanf("%d", &b);
 
+	for (a = -2147483648; a <= 2147483647; a++) {
+        for (b = -2147483648; b <= 2147483647; b++) {
+            if (!mul_check(a, b, n)) {
+                printf("\nError:\n");
+                printf("%d x %d\n", a, b);
+                return 1;
+            }
+            // 進捗を表示
+        	printf("\033[F");
+        	printf("\033[K");
+        	printf("\033[F");
+        	printf("\033[K");
 
+        	printf("a:%d\n", a);
+        	printf("b:%d\n", b);
+        }
+        
+        // printf("%d\n", a);
+    }
+
+	printf("\nDone.\n");
+
+	return 0;
+}
+
+bool mul_check(int a, int b, int n[32]){
 	// bool型のa, b
 	bool bool_a[32], bool_b[32];
 
@@ -37,7 +60,7 @@ int main(void){
 		q=0;
 		for(int j=0; j < 32; j++){
 			r = ((int)(bool_a[i] && bool_b[j])) << j;
-			// 各桁の最後の値or最後のbの最後のa以外を負の数にする
+			// 各桁の最後の値or
 			if((j == 31 && i != 31) || (j != 31 && i == 31)){
 				r = -r;
 			}
@@ -47,9 +70,11 @@ int main(void){
 		p += q;
 	}
 
-	// 乗算結果の出力
-	printf("%d x %d = %d\n", a, b, p);
-	printf("\nDone!\n");
+	int x = a * b;
 
-	return 0;
+	if(x - p == 0){
+		return true;
+	}
+
+	return false;
 }
